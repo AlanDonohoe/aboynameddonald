e02943f4c9a74410e5d68f8b51c17fbc4ca04b55
Alan Donohoe <alandonohoe@hotmail.com>
2021-09-26 15:38:00 +0100

    Update npm packages.

--- public/assets/scripts/bundle.js
+++ public/assets/scripts/bundle.js
@@ -1,99 +1,21 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/assets";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ({
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/scripts/main.js":
 /*!*****************************!*\
   !*** ./src/scripts/main.js ***!
   \*****************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (() => {
 
-eval("var modal = document.querySelector('.modal');\nvar modalOverlay = document.querySelector('.modal-overlay');\nvar buttonClose = document.querySelector('.modal-close-button');\nvar buttonOpen = document.querySelector('.modal-open-button');\nvar videoPlayer = document.querySelector('.video-player');\n\nvar hideModalPauseVideo = function hideModalPauseVideo() {\n  modal.classList.toggle('closed');\n  modalOverlay.classList.toggle('closed');\n  videoPlayer.pause();\n};\n\nvar showModalPlayVideo = function showModalPlayVideo() {\n  modal.classList.toggle('closed');\n  modalOverlay.classList.toggle('closed');\n  videoPlayer.play();\n};\n\nbuttonClose.addEventListener('click', function () {\n  hideModalPauseVideo();\n});\nbuttonOpen.addEventListener('click', function () {\n  showModalPlayVideo();\n});\nmodalOverlay.addEventListener('click', function () {\n  hideModalPauseVideo();\n});\n\n//# sourceURL=webpack:///./src/scripts/main.js?");
+eval("const modal = document.querySelector('.modal');\nconst modalOverlay = document.querySelector('.modal-overlay');\nconst buttonClose = document.querySelector('.modal-close-button');\nconst buttonOpen = document.querySelector('.modal-open-button');\nconst videoPlayer = document.querySelector('.video-player');\n\nconst hideModalPauseVideo = function () {\n  modal.classList.toggle('closed');\n  modalOverlay.classList.toggle('closed');\n  videoPlayer.pause();\n};\n\nconst showModalPlayVideo = function () {\n  modal.classList.toggle('closed');\n  modalOverlay.classList.toggle('closed');\n  videoPlayer.play();\n};\n\nbuttonClose.addEventListener('click', function () {\n  hideModalPauseVideo();\n});\nbuttonOpen.addEventListener('click', function () {\n  showModalPlayVideo();\n});\nmodalOverlay.addEventListener('click', function () {\n  hideModalPauseVideo();\n});\n\n//# sourceURL=webpack://a_boy_named_donald_site/./src/scripts/main.js?");
 
 /***/ }),
 
@@ -101,22 +23,46 @@ eval("var modal = document.querySelector('.modal');\nvar modalOverlay = document
 /*!******************************!*\
   !*** ./src/styles/main.scss ***!
   \******************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (() => {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/styles/main.scss?");
-
-/***/ }),
-
-/***/ 0:
-/*!**********************************************************!*\
-  !*** multi ./src/scripts/main.js ./src/styles/main.scss ***!
-  \**********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("__webpack_require__(/*! ./src/scripts/main.js */\"./src/scripts/main.js\");\nmodule.exports = __webpack_require__(/*! ./src/styles/main.scss */\"./src/styles/main.scss\");\n\n\n//# sourceURL=webpack:///multi_./src/scripts/main.js_./src/styles/main.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack://a_boy_named_donald_site/./src/styles/main.scss?");
 
 /***/ })
 
-/******/ });
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_require__("./src/scripts/main.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/styles/main.scss");
+/******/ 	
+/******/ })()
+;
